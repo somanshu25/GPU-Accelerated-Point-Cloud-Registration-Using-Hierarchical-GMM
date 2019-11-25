c69ada6114ff92541a647de7e0c42b5ba20a9c05
Srinath Rajagopalan <srinath132@gmail.com>
2019-11-24 22:24:55 -0500

    separate modules.

--- scan_matching/mlp.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "common.h"
-
-namespace CharacterRecognition {
-    Common::PerformanceTimer& timer();
-
-    // TODO: implement required elements for MLP sections 1 and 2 here
-}
